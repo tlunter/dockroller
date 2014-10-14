2431b200a8fb71bda62df6ebd0f99bf55d4968df
Todd Lunter <tlunter@gmail.com>
2014-10-13 21:57:20 -0700

    Not exactly static, but builds socket
    Does not build another socket if one is already associated with this
    server and tracks the lastest socket per person

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,26 @@
+require 'securerandom'
+
 class StaticController < ApplicationController
   def index
     base.erb :index
+  end
+
+  def socket
+    socket = base.session['socket'] ||= SecureRandom.hex(32)
+    base.request.websocket do |ws|
+      ws.onopen do
+        base.settings.sockets[socket] = ws
+        ws.send("test")
+      end
+      ws.onmessage do |msg|
+        EM.next_tick {
+          base.settings.sockets[socket].send(msg)
+        }
+      end
+      ws.onclose do
+        base.settings.sockets.delete(socket)
+        timer.cancel
+      end
+    end
   end
 end
